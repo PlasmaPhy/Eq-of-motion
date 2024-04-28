3f08f8ae7158a69a01e68ae3ce4162ec93be6241
GeorgiaZ9 <87611897+GeorgiaZ9@users.noreply.github.com>
2024-04-28 15:47:46 +0300

    Organized and updated script

--- Paper_equations.py
+++ Paper_equations.py
@@ -12,15 +12,8 @@ def calc_hyp2f1(psi, q_0, q_wall, n):
     d = (1 - (q_wall/q_0)**n) * (psi)**n
     return hyp2f1(a, b, c, d)
 
-def runge_kutta4(f, t, y, h, m, q_0):
-    k1 = h * f(t, y, m, q_0)
-    k2 = h * f(t + 0.5 * h, y + 0.5 * k1, m, q_0)
-    k3 = h * f(t + 0.5 * h, y + 0.5 * k2, m, q_0)
-    k4 = h * f(t + h, y + k3, m, q_0)
-    return y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-
 def solve_with_odeint(f, t0, y0, tf, h, m, q_0):
-    t_values = np.linspace(t0, tf, int(tf/h) + 1)  # Create time grid with more precision
+    t_values = np.linspace(t0, tf, int(tf/h) + 1)  
     return odeint(f, y0, t_values, args=(m, q_0))
 
 def paper_differential_equations(y, t, m, q_0):
@@ -34,23 +27,33 @@ def paper_differential_equations(y, t, m, q_0):
     n = 2
 
     psi_p = psi/q_0*calc_hyp2f1(psi, q_0, q_wall, n)
-    q = 1 #q_0 *(1 + (psi)**2)**(1/2)
+    q = q_0 *(1 + (psi)**2)**(1/2)
 
-    der_theta = - ((1-(2*psi)*np.cos(theta))*(P_J + psi_p))/np.sqrt(2*psi)*np.cos(theta) - m*np.cos(theta)/np.sqrt(2*psi) + (1-psi*np.cos(theta))**2*(P_J +psi_p)*q
-    der_psi = -(P_J + psi_p)**2*(1 - np.sqrt(psi))*np.sqrt(psi)*np.sin(theta) - m*psi*np.sin(theta)
+    der_theta = - ((1-np.sqrt(2*psi)*np.cos(theta))*(P_J + psi_p))**2/np.sqrt(2*psi)*np.cos(theta) - m*np.cos(theta)/np.sqrt(2*psi) + (1 - np.sqrt(2*psi)*np.cos(theta))**2*(P_J +psi_p)/q
+    der_psi = -(P_J + psi_p)**2*(1 - np.sqrt(2*psi)*np.cos(theta))*np.sqrt(2*psi)*np.sin(theta) - m*np.sqrt(2*psi)*np.sin(theta)
     der_P_J = 0
-    der_J = (P_J + psi_p)*(1 - psi*np.cos(theta))
+    der_J = (P_J + psi_p)*(1 - np.sqrt(2*psi)*np.cos(theta))**2
 
     return np.array([der_P_J, der_psi, der_J, der_theta])
 
 
-def solve_runge_kutta4(f, t0, y0, tf, h, m, q_0):
-    t_values = np.arange(t0, tf + h, h)
-    y_values = [y0]
-    t = t0
-    y = y0
-    for next_t in t_values[1:]:
-        y = runge_kutta4(f, t, y, h, m, q_0)
-        t = next_t
-        y_values.append(y)
-    return np.array(t_values), np.array(y_values)
+# alternative Runge-Kutta function
+
+# def solve_runge_kutta4(f, t0, y0, tf, h, m, q_0):
+#     t_values = np.arange(t0, tf + h, h)
+#     y_values = [y0]
+#     t = t0
+#     y = y0
+#     for next_t in t_values[1:]:
+#         y = runge_kutta4(f, t, y, h, m, q_0)
+#         t = next_t
+#         y_values.append(y)
+#     return np.array(t_values), np.array(y_values)
+
+
+# def runge_kutta4(f, t, y, h, m, q_0):
+#     k1 = h * f(t, y, m, q_0)
+#     k2 = h * f(t + 0.5 * h, y + 0.5 * k1, m, q_0)
+#     k3 = h * f(t + 0.5 * h, y + 0.5 * k2, m, q_0)
+#     k4 = h * f(t + h, y + k3, m, q_0)
+#     return y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
