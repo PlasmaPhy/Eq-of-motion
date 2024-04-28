Updated scripts, corrected errors in derivatives

Paper_equations.py
@@ -29,7 +29,7 @@ def paper_differential_equations(y, t, m, q_0):
     psi_p = psi/q_0*calc_hyp2f1(psi, q_0, q_wall, n)
     q = q_0 *(1 + (psi)**2)**(1/2)
 
-    der_theta = - ((1-np.sqrt(2*psi)*np.cos(theta))*(P_J + psi_p))**2/np.sqrt(2*psi)*np.cos(theta) - m*np.cos(theta)/np.sqrt(2*psi) + (1 - np.sqrt(2*psi)*np.cos(theta))**2*(P_J +psi_p)/q
+    der_theta = - np.cos(theta)*(1-np.sqrt(2*psi)*np.cos(theta))*((P_J + psi_p))**2/np.sqrt(2*psi) - m*np.cos(theta)/np.sqrt(2*psi) + (1 - np.sqrt(2*psi)*np.cos(theta))**2*(P_J +psi_p)/q
     der_psi = -(P_J + psi_p)**2*(1 - np.sqrt(2*psi)*np.cos(theta))*np.sqrt(2*psi)*np.sin(theta) - m*np.sqrt(2*psi)*np.sin(theta)
     der_P_J = 0
     der_J = (P_J + psi_p)*(1 - np.sqrt(2*psi)*np.cos(theta))**2