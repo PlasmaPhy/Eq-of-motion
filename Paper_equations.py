bb3cfafd4277a6e4b67c7912273cf11f556a907b
GeorgiaZ9 <87611897+GeorgiaZ9@users.noreply.github.com>
2024-07-11 19:59:51 +0300

    changed parameters on the equations of motion

--- Paper_equations.py
+++ Paper_equations.py
@@ -12,22 +12,24 @@ def calc_hyp2f1(psi, q_0, q_wall, n):
     d = (1 - (q_wall/q_0)**n) * (psi)**n
     return hyp2f1(a, b, c, d)
 
-def solve_with_odeint(f, t0, y0, tf, h, m, q_0):
+def solve_with_odeint(f, t0, y0, tf, h, m, q_wall, q_0, n):
     t_values = np.linspace(t0, tf, int(tf/h) + 1)  
-    return odeint(f, y0, t_values, args=(m, q_0))
+    return odeint(f, y0, t_values, args=(m, q_wall, q_0, n))
 
-def paper_differential_equations(y, t, m, q_0):
+def paper_differential_equations(y, t, m, q_wall, q_0, n):
+    # y      : solution for the ode 
+    # t      : time parameter
+    # m      : magnetic moment of order 10^-5
+    # q_wall : 
+    # q_0    : we consider it 1
+
     P_J = y[0]
     psi = y[1]
     J = y[2]
     theta = y[3]
-    
-    # hypergeometric function parameters definition
-    q_wall = 3.5
-    n = 2
 
     psi_p = psi/q_0*calc_hyp2f1(psi, q_0, q_wall, n)
-    q = q_0 *(1 + (psi)**2)**(1/2)
+    q = q_0*(1 + (psi)**2)**(1/2)
 
     der_theta = - np.cos(theta)*(1-np.sqrt(2*psi)*np.cos(theta))*((P_J + psi_p))**2/np.sqrt(2*psi) - m*np.cos(theta)/np.sqrt(2*psi) + (1 - np.sqrt(2*psi)*np.cos(theta))**2*(P_J +psi_p)/q
     der_psi = -(P_J + psi_p)**2*(1 - np.sqrt(2*psi)*np.cos(theta))*np.sqrt(2*psi)*np.sin(theta) - m*np.sqrt(2*psi)*np.sin(theta)
@@ -36,24 +38,3 @@ def paper_differential_equations(y, t, m, q_0):
 
     return np.array([der_P_J, der_psi, der_J, der_theta])
 
-
-# alternative Runge-Kutta function
-
-# def solve_runge_kutta4(f, t0, y0, tf, h, m, q_0):
-#     t_values = np.arange(t0, tf + h, h)
-#     y_values = [y0]
-#     t = t0
-#     y = y0
-#     for next_t in t_values[1:]:
-#         y = runge_kutta4(f, t, y, h, m, q_0)
-#         t = next_t
-#         y_values.append(y)
-#     return np.array(t_values), np.array(y_values)
-
-
-# def runge_kutta4(f, t, y, h, m, q_0):
-#     k1 = h * f(t, y, m, q_0)
-#     k2 = h * f(t + 0.5 * h, y + 0.5 * k1, m, q_0)
-#     k3 = h * f(t + 0.5 * h, y + 0.5 * k2, m, q_0)
-#     k4 = h * f(t + h, y + k3, m, q_0)
-#     return y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
